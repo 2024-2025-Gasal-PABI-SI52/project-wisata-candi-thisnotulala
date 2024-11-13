f60355c13b41be6a071f8635014066845855649e
Cesya Catherine <cesyacatherine_2226240018@mhs.mdp.ac.id>
2024-11-13 18:37:02 +0700

    commit
    commit

--- wisata_candi/lib/screens/profile_screen.dart
+++ wisata_candi/lib/screens/profile_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatefulWidget{
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
@@ -15,15 +15,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
   int favoriteCandiCount = 0;
 
   //TODO: 5. Implementasi fungsi sign in
-  void signIn(){
+  void signIn() {
     setState(() {
       isiSignedIn = !isiSignedIn;
     });
   }
 
-
   //TODO: 6. Implementasu fungsi sign out
-  void signOut(){
+  void signOut() {
     setState(() {
       isiSignedIn = !isiSignedIn;
     });
@@ -35,17 +34,19 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: Stack(
         children: [
           Container(
-            height: 200, width: double.infinity, color: Colors.deepPurple,
+            height: 200,
+            width: double.infinity,
+            color: Colors.deepPurple,
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal:16),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               children: [
                 //TODO: 2. buat bgian profile header (isi gmbar profile)
                 Align(
                   alignment: Alignment.topCenter,
                   child: Padding(
-                    padding: const EdgeInsets.only(top:150),
+                    padding: const EdgeInsets.only(top: 150),
                     child: Stack(
                       alignment: Alignment.bottomRight,
                       children: [
@@ -60,17 +61,17 @@ class _ProfileScreenState extends State<ProfileScreen> {
                           child: const CircleAvatar(
                             radius: 50,
                             backgroundImage:
-                              AssetImage('images/placeholder_image.png'),
+                                AssetImage('images/placeholder_image.png'),
                           ),
                         ),
-                        if(isiSignedIn)
-                        IconButton(
-                          onPressed: (){}, 
-                          icon: Icon(
-                            Icons.camera_alt,
-                            color: Colors.deepPurple[50],
+                        if (isiSignedIn)
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.camera_alt,
+                              color: Colors.deepPurple[50],
+                            ),
                           ),
-                        ),
                       ],
                     ),
                   ),
@@ -78,7 +79,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 //TODO: 3. buat profile info (isi info profile)
                 //baris 1 pengguna
                 const SizedBox(
-                  height:  20,
+                  height: 20,
                 ),
                 Divider(
                   color: Colors.deepPurple[100],
@@ -89,34 +90,32 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 Row(
                   children: [
                     SizedBox(
-                      width: MediaQuery.of(context).size.width/3,
+                      width: MediaQuery.of(context).size.width / 3,
                       child: const Row(
                         children: [
                           Icon(
-                            Icons.lock, 
+                            Icons.lock,
                             color: Colors.amber,
                           ),
                           SizedBox(
                             width: 8,
                           ),
                           Text(
-                            'Pengguna', 
+                            'Pengguna',
                             style: TextStyle(
-                              fontSize: 18, 
-                              fontWeight: FontWeight.bold
-                            ),
+                                fontSize: 18, fontWeight: FontWeight.bold),
                           ),
                         ],
                       ),
                     ),
                     Expanded(
-                      child: Text(': $userName', 
-                      style: TextStyle(fontSize: 18),
+                      child: Text(
+                        ': $userName',
+                        style: TextStyle(fontSize: 18),
                       ),
                     ),
                   ],
                 ),
-
 
                 //baris 2 nama
                 const SizedBox(
@@ -131,36 +130,33 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 Row(
                   children: [
                     SizedBox(
-                      width: MediaQuery.of(context).size.width/3,
+                      width: MediaQuery.of(context).size.width / 3,
                       child: const Row(
                         children: [
                           Icon(
-                            Icons.person, 
+                            Icons.person,
                             color: Colors.blue,
                           ),
                           SizedBox(
                             width: 8,
                           ),
                           Text(
-                            'Nama', 
+                            'Nama',
                             style: TextStyle(
-                              fontSize: 18, 
-                              fontWeight: FontWeight.bold
-                            ),
+                                fontSize: 18, fontWeight: FontWeight.bold),
                           ),
                         ],
                       ),
                     ),
                     Expanded(
-                      child: Text(': $fullName', 
+                        child: Text(
+                      ': $fullName',
                       style: TextStyle(fontSize: 18),
-                      )
-                    ),
-                    if(isiSignedIn) const Icon(Icons.edit),
+                    )),
+                    if (isiSignedIn) const Icon(Icons.edit),
                   ],
                 ),
 
-                
                 //baris 3 favorit
                 const SizedBox(
                   height: 4,
@@ -174,29 +170,28 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 Row(
                   children: [
                     SizedBox(
-                      width: MediaQuery.of(context).size.width/3,
+                      width: MediaQuery.of(context).size.width / 3,
                       child: const Row(
                         children: [
                           Icon(
-                            Icons.favorite, 
+                            Icons.favorite,
                             color: Colors.red,
                           ),
                           SizedBox(
                             width: 8,
                           ),
                           Text(
-                            'Favorit', 
+                            'Favorit',
                             style: TextStyle(
-                              fontSize: 18, 
-                              fontWeight: FontWeight.bold
-                            ),
+                                fontSize: 18, fontWeight: FontWeight.bold),
                           ),
                         ],
                       ),
                     ),
                     Expanded(
-                      child: Text(': $favoriteCandiCount', 
-                      style: TextStyle(fontSize: 18),
+                      child: Text(
+                        ': $favoriteCandiCount',
+                        style: TextStyle(fontSize: 18),
                       ),
                     ),
                   ],
@@ -212,15 +207,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 const SizedBox(
                   height: 20,
                 ),
-                isiSignedIn 
-                ? TextButton(
-                  onPressed: signOut, child: const Text('Sign Out'))
-                : TextButton(onPressed: signIn, child: const Text('Sign In'))
+                isiSignedIn
+                    ? TextButton(
+                        onPressed: signOut, child: const Text('Sign Out'))
+                    : TextButton(
+                        onPressed: signIn, child: const Text('Sign In'))
               ],
             ),
           ),
         ],
-      ),
-    );
-  }
+      ),
+    );
+  }
 }
